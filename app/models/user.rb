f2d5d9508b857cd59ca4159b5601d725b3f00b9c
p-duke <peteraaronduke@gmail.com>
2017-05-20 17:37:44 -0400

    Add User functionality with tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User
   include ActiveModel::Model
 
+  EAST_COST_LOCATIONS = [ 'Boston', 'New York' ].freeze
+
   attr_accessor :name, :age, :gender, :location, :health_conditions
 
   validates :name, :age, :gender, :location, presence: true
@@ -13,6 +15,28 @@ class User
     @gender = params[:gender]
     @location = params[:location]
     @health_conditions = params[:health_conditions]
+  end
+
+  def over_18?
+    self.age > 18
+  end
+
+  def number_of_years_over_18
+    self.age - 18
+  end
+
+  def east_coast_location?
+    EAST_COST_LOCATIONS.any? {|city| format_location(city) == format_location(self.location) }
+  end
+
+  def is_female?
+    self.gender == 'female'
+  end
+
+  private
+
+  def format_location(location)
+    location.gsub(/\s+/,"").downcase
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model  do
 
-  params = {name: 'Charlie', age: 50, gender: 'male', location: 'California', health_conditions: ["Heart Disease", "High Cholesterol"]} 
+  params = {name: 'Charlie', age: 22, gender: 'female', location: 'Boston', health_conditions: ["Heart Disease", "High Cholesterol"]}
   subject(:user) { User.new(params) }
     
   describe 'initialize' do
 
     it { is_expected.to have_attributes(name: 'Charlie') }
-    it { is_expected.to have_attributes(age: 50) }
-    it { is_expected.to have_attributes(gender: 'male') }
-    it { is_expected.to have_attributes(location: 'California') }
+    it { is_expected.to have_attributes(age: 22) }
+    it { is_expected.to have_attributes(gender: 'female') }
+    it { is_expected.to have_attributes(location: 'Boston') }
     it { is_expected.to have_attributes(health_conditions: ['Heart Disease', 'High Cholesterol'] ) }
 
   end
@@ -50,6 +50,60 @@ RSpec.describe User, type: :model  do
       before { subject.gender = "taco" }
 
       it { should_not be_valid }
+    end
+  end
+
+  describe '#number_of_years_over_18' do
+    it 'should return the number of years over' do
+      expect(user.number_of_years_over_18).to eq(4)
+    end
+  end
+
+  describe '#over_18?' do
+    context 'when the users age is > 18' do
+      it 'should be truthy' do
+        expect(user.over_18?).to be_truthy
+      end
+    end
+
+    context 'when the users age is <= 18' do
+      before { user.age = 17 }
+
+      it 'should be falsey' do
+        expect(user.over_18?).to be_falsey
+      end
+    end
+  end
+
+  describe '#east_coast_location?' do
+    context 'when the users location is on the east coast' do
+      it 'should be truthy' do
+        expect(user.east_coast_location?).to be_truthy
+      end
+    end
+
+    context 'when the users location is not on the east coast' do
+      before { user.location = 'Mars' }
+
+      it 'should be falsey' do
+        expect(user.east_coast_location?).to be_falsey
+      end
+    end
+  end
+
+  describe '#is_female?' do
+    context 'when the users gender is female' do
+      it 'should be truthy' do
+        expect(user.is_female?).to be_truthy
+      end
+    end
+
+    context 'when the users gender is not female' do
+      before { user.gender = 'male' }
+
+      it 'should be falsey' do
+        expect(user.is_female?).to be_falsey
+      end
     end
   end
 end
