06e2c40facb36326a0cce273466d92925e65ab1a
p-duke <peteraaronduke@gmail.com>
2017-05-22 00:11:22 -0400

    fixup! Reconfigure rails loading

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-#require "rails"
+require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-#require "active_job/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
-#require "action_mailer/railtie"
+require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
@@ -17,6 +17,9 @@ Bundler.require(*Rails.groups)
 
 module PgCodeChallenge
   class Application < Rails::Application
+    # Use the responders controller from the responders gem
+    config.app_generators.scaffold_controller :responders_controller
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
