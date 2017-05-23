b27e538b30b78991c2bedc3075bc2db32651a4fb
p-duke <peteraaronduke@gmail.com>
2017-05-23 00:06:45 -0400

    Create PriceCalculator helper to parse params

--- app/helpers/price_calculations_helper.rb
+++ app/helpers/price_calculations_helper.rb
@@ -1,2 +1,34 @@
 module PriceCalculationsHelper
+
+  def create_policy_calculations(params)
+    policy_calculations = []
+    users = parse_params(params)
+    users.each do |user|
+      new_user = User.new(user)
+      price_calc = PriceCalculation.new(user: new_user)
+      price_calc.run_policy_calculation
+      policy_calculations << [new_user, price_calc]
+    end
+
+    policy_calculations
+  end
+
+  def parse_params(params)
+   users = []
+   user_params = params["info"].split("\r\n")
+   header = user_params.shift.split("|").map(&:strip)
+   header.map! {|title| title.parameterize.underscore}
+   user_data = user_params.map {|param| param.split("|").map(&:strip)}
+
+   user_data.each do |data|
+     users << header.zip(data).to_h
+   end
+
+   users.each_with_index.map do |key,val|
+     key["health_conditions"]= key["health_conditions"].split(',').map(&:strip)
+     key["age"] = key["age"].to_i
+   end
+
+   users
+  end
 end
